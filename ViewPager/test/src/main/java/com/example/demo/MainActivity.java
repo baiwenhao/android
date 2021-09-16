d9d3407596ea460d0fb2d8eff9a07ff9d4b8688e
wenhao <bwh2009@163.com>
2021-09-16 17:12:22 +0800

    add vertical menu

--- ViewPager/test/src/main/java/com/example/demo/MainActivity.java
+++ ViewPager/test/src/main/java/com/example/demo/MainActivity.java
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
+/**
+ * @author wenhao
+ */
 public class MainActivity extends AppCompatActivity {
+    LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +20,16 @@ public class MainActivity extends AppCompatActivity {
         // 为ViewPager构建Adapter
 
         ViewPager2 viewPager = findViewById(R.id.viewPager);
-        MyAdapter viewPagerAdapter = new MyAdapter(); // 创建适配器
+        MyAdapter viewPagerAdapter = new MyAdapter();
         viewPager.setAdapter(viewPagerAdapter);
+
+//        linearLayout = findViewById(R.id.btn);
+//        linearLayout.setOnClickListener((v)-> {
+//            linearLayout.setActivated(true);
+//        });
+
+        // 动态设置背景色
+//        viewPager.setBackgroundColor(Color.parseColor("#FF00FF"));
+//        viewPager.setBackgroundResource(R.drawable.main_button);
     }
 }
--- ViewPager/verticaltab/src/main/java/com/example/verticaltab/MainActivity.java
+++ ViewPager/verticaltab/src/main/java/com/example/verticaltab/MainActivity.java
@@ -8,16 +8,22 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
-import android.view.animation.BounceInterpolator;
 import android.view.animation.OvershootInterpolator;
 import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
+
+/**
+ * @author wenhao
+ */
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     ViewPager2 viewPager;
-    String TAG = "home";
+    String tag = "home";
     ConstraintLayout i1, i2, i3;
-    LinearLayout active;
+    ImageView i1Img,i2Img,i3Img;
+    RelativeLayout active;
     int prevTop = 0;
 
     @Override
@@ -31,6 +37,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void initEvent() {
         viewPager = findViewById(R.id.view_page);
         active = findViewById(R.id.active);
+        i1Img = findViewById(R.id.i1_img);
+        i2Img = findViewById(R.id.i2_img);
+        i3Img = findViewById(R.id.i3_img);
+
         i1 = findViewById(R.id.i1);
         i2 = findViewById(R.id.i2);
         i3 = findViewById(R.id.i3);
@@ -38,29 +48,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         i1.setOnClickListener(this);
         i2.setOnClickListener(this);
         i3.setOnClickListener(this);
-        Log.d(TAG, "init");
+        Log.d(tag, "init");
+
     }
 
     @Override
     public void onClick(View v) {
-        Log.d(TAG, "click");
+        Log.d(tag, "click");
         changeTab(v.getId());
     }
 
-    public void changeTab (int position) {
-        switch (position) {
-            case R.id.i1:
-                start(active, i1.getTop());
-                prevTop = i1.getTop();
-                break;
-            case R.id.i2:
-                start(active, i2.getTop());
-                prevTop = i2.getTop();
-                break;
-            case R.id.i3:
-                start(active, i3.getTop());
-                prevTop = i3.getTop();
-                break;
+    public void changeTab (int id) {
+        if (id == R.id.i1) {
+            i1Img.setActivated(true);
+            start(active, i1.getTop());
+            prevTop = i1.getTop();
+        } else if (id == R.id.i2) {
+            i2Img.setActivated(true);
+            start(active, i2.getTop());
+            prevTop = i2.getTop();
+        } else if (id == R.id.i3) {
+            i3Img.setActivated(true);
+            start(active, i3.getTop());
+            prevTop = i3.getTop();
         }
     }
 
@@ -75,7 +85,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //        Interpolator interpolator = new AnticipateOvershootInterpolator();//开始之前向前甩，结束的时候向后甩
         view.setVisibility(View.VISIBLE);
         TranslateAnimation ani = new TranslateAnimation(0,0,prevTop,end);
-        Log.d(TAG, "起点=" + prevTop + "-" + "终点=" + end);
+        Log.d(tag, "起点=" + prevTop + "-" + "终点=" + end);
         ani.setInterpolator(new OvershootInterpolator(1));
         ani.setDuration(200);
         ani.setFillEnabled(true);
@@ -85,18 +95,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ani.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
-                Log.d(TAG, "begin");
+                Log.d(tag, "begin");
             }
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                Log.d(TAG, "finish");
+                Log.d(tag, "finish");
 //                view.clearAnimation();
             }
 
             @Override
             public void onAnimationRepeat(Animation animation) {
-                Log.d(TAG, "repeat");
+                Log.d(tag, "repeat");
             }
         });
     }
